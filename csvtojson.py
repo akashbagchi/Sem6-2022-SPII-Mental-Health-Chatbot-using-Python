4f66b815462c6554e610850278902d3cd3c77315
Akash Bagchi <akashbagchi2710@gmail.com>
2022-05-08 20:38:07 +0530

    Changed to create JSON output file that is more usable with NLTK

--- csvtojson.py
+++ csvtojson.py
@@ -2,12 +2,18 @@ import os
 import json
 import csv
 data_dict = {}
+data_dict['intents'] = []
 
 with open('./Datasets/Mental_Health_FAQ.csv', encoding = 'utf-8') as csv_file_handler:
     csv_reader = csv.DictReader(csv_file_handler)
     for row in csv_reader:
-        key = row['Question_ID']
-        data_dict[key] = row
+        row['tag'] = row['Question_ID']
+        del(row['Question_ID'])
+        row['patterns'] = row['Questions']
+        del(row['Questions'])
+        row['responses'] = row['Answers']
+        del(row['Answers'])
+        data_dict['intents'].append(row)
 
 with open('./JSON/faq.json', 'w', encoding = 'utf-8') as json_file_handler:
     json_file_handler.write(json.dumps(data_dict, indent = 4))
